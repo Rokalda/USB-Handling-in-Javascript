77d4a93c17e0ca9d5a98f298def2c14dcd9c3fff
Rokalda <nikolai.scarlett53@gmail.com>
2024-06-10 16:27:56 -0500

    MAJOR, UPDATE, The Property Dialog is in full effect, I altered the addDevice method to accomadate.

--- usb.js
+++ usb.js
@@ -2,6 +2,15 @@
 if("usb" in navigator){
  
 const prop_modal=  document.getElementById("property_dialog")
+prop_modal.querySelector("h3").onclick=()=>{
+prop_modal.querySelectorAll("input[type=checkbox]").forEach((chbox)=>{
+  if(!chbox.checked){
+    chbox.click()
+  }
+})
+}
+const table=document.querySelector("table");
+const tbody=table.querySelector("tbody");
     // First fill the table with all prevoisly paired devices;
     let paired_devices=[]
     // These are the USB Device Propeties that can be shown on 
@@ -18,12 +27,34 @@ const prop_modal=  document.getElementById("property_dialog")
       {prop_name:"deviceSubclass",display_name:"Sub Class Code",shown:false},
       {prop_name:"deviceVersionMajor",display_name:"Device Ver (MAJ)",shown:false},
       {prop_name:"deviceVersionMinor",display_name:"Device Ver (MIN)",shown:false},
+      {prop_name:"usbVersionMajor",display_name:"USB Ver (MAJ)",shown:false},
+      {prop_name:"usbVersionMinor",display_name:"USB Ver (MIN)",shown:false},
+
+
 
 
     
     ]
+    function hide_or_filterColumns(e){
+        let isShown=e.target.checked;
+        let col_name=e.target.id;
+        const affected_col= table.tHead.querySelector(`th.${col_name}`)
+       
+        if(isShown){
+          affected_col.scrollIntoView({behavior:"smooth"})
+        }
+        affected_col.hidden=!isShown;
 
+        tbody.querySelectorAll(`td.${col_name}`).forEach((td)=>{
+          td.hidden=!isShown;
+        });
+
+
+       
+    }
+    //Loop Through all deez properties
     usb_properties.forEach(prop=>{
+   
     const propElement=  document.createElement("div");
     propElement.className="prop"
     
@@ -32,12 +63,19 @@ const prop_modal=  document.getElementById("property_dialog")
       <input type="checkbox" name="${prop.prop_name}" id="${prop.prop_name}">
       ${prop.display_name}
     </label>`
-    propElement.querySelector("input").checked=prop.shown;
 
+    propElement.querySelector("input").checked=prop.shown;
+    propElement.querySelector("input").onchange=hide_or_filterColumns
       prop_modal.querySelector(".properties").appendChild(propElement)
+      // After adding the property checkboxes, Adding the header columns
+
+      const new_th=document.createElement("th");
+      new_th.hidden=!prop.shown;
+      new_th.className=prop.prop_name;
+      new_th.textContent=prop.display_name;
+      table.tHead.querySelector("tr").appendChild(new_th)
     })
-    const table=document.querySelector("table");
-    const tbody=table.querySelector("tbody");
+
 
     navigator.usb.onconnect=(e)=>{
       addDevice(e.device)
@@ -61,10 +99,19 @@ const filter_inputs=document.querySelector(".filter_box").querySelectorAll("inpu
   const prop_btn = document.querySelector(".prop_btn")
   req_btn.addEventListener("click",requestUSBDevice);
 
-  prop_modal.querySelector("#close").onclick=()=>prop_modal.close()
+  prop_modal.querySelector("#close").onclick=closePropertyDialog
   
+function closePropertyDialog(){
+prop_modal.classList.remove("open")
+}
 
-prop_btn.onclick=()=>  prop_modal.showModal()
+function showPropertyDialog(){
+table.scrollIntoView()
+prop_modal.classList.add("open")
+
+}
+
+prop_btn.onclick=showPropertyDialog
 
 
 
@@ -114,26 +161,27 @@ filter_inputs.forEach((input)=>{
     paired_devices.push(device)
     console.log(paired_devices)
     update_ui()
-    let tr = 
-    `<tr id="${device.vendorId}-${device.productId}">
-        <td>${device.productId}</td>
-        <td>${device.vendorId}</td>
-        <td>${device.deviceClass}</td>
-        <td>${device.manufacturerName}</td>
-        <td>${device.serialNumber}</td>
-        <td>${device.productName}</td>
+    // Create the Device Row
+    let tr = document.createElement("tr")
+    tr.id=`${device.vendorId}-${device.productId}`
+  
+    usb_properties.forEach((prop)=>{
+     const td=document.createElement("td")
+     td.className=prop.prop_name
+     td.hidden=!prop.shown;
+     td.textContent=device[prop.prop_name]
+     tr.appendChild(td)
+    })
 
-    </tr>`
-
-    tr=tr.replaceAll("undefined","N/A")
-  tbody.innerHTML+=tr;
+  
+  tbody.appendChild(tr);
   }
 
   function removeDevice(device){
     const r_index=paired_devices.indexOf(device);
     paired_devices.splice(r_index,1)
     update_ui()
-    tbody.removeChild(tbody.querySelector(`tr[id="${device.vendorId}-${device.productId}"]`))
+    tbody.querySelector(`tr[id="${device.vendorId}-${device.productId}"]`).remove()
   }
 }
 else{
